0447c04d332e11425044487133b9ede6075fa0ae
qingsuijiu <55686527+qingsuijiu@users.noreply.github.com>
2021-07-28 17:17:16 +0800

    Delete SpringBoot-Blog-master directory

--- SpringBoot-Blog-master/src/main/java/com/my/blog/website/service/IndexService.java
+++
@@ -1,9 +0,0 @@
-package com.my.blog.website.service;
-
-import com.my.blog.website.model.Bo.ArticleBo;
-
-import java.util.List;
-
-public interface IndexService {
-    List<ArticleBo> selectAll();
-}
